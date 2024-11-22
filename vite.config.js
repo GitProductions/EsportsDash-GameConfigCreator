2b3ce26bcabf7c989a3530e8e12e9b3279fe0987
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-11-22 14:22:23 -0500

    Update configDownloader.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
 export default defineConfig({
-  // base: '/EsportsDash-GameConfigCreator/',
-  base: '/',
+
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['bootstrap']
-    }
-  }
-})
+  base: '/',
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   // base: '/EsportsDash-GameConfigCreator/',
+//   base: '/',
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['bootstrap']
+//     }
+//   }
+// })
