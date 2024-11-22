cea99a0784085ad47e6d90b421d6d9bb5508fc55
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-11-22 17:26:48 -0500

    react-dom reinstall?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
+
   plugins: [react()],
   base: '/EsportsDash-GameConfigCreator/',
 });
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/EsportsDash-GameConfigCreator/',
+//   // base: '/',
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['bootstrap']
+//     }
+//   }
+// })
