2afaab2f1e5ed2ee49463a9288146bb8ac51bffd
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-11-22 17:24:55 -0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavigationProvider } from './context/NavigationContext';
 
 import Header from './Header';
