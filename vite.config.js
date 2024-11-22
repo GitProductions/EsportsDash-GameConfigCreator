34d8765a4e1ea18a1c95de650aade2469633677d
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-11-22 17:14:36 -0500

    added /EsportsDash-GameConfigCreator/ base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 
   plugins: [react()],
-  base: '/',
+  base: '/EsportsDash-GameConfigCreator/',
 });
 
 // import { defineConfig } from 'vite'
